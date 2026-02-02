03ec6afd91c2326fe7b831a6c2f78b50e71314d3
Karan Singh Shekhawat <shekhawatk271@gamil.com>
2026-02-02 11:24:41 +0530

    setup frontend UI structure and backend

---
+++ RESUMATE/src/App.tsx
@@ -0,0 +1,14 @@
+ import React from 'react'
+ 
+ const App = () => {
+   return (
+     <div className="flex items-center justify-center h-screen bg-gray-100">
+
+      heyy
+       
+     </div>
+   )
+ }
+ 
+ export default App
+ 
